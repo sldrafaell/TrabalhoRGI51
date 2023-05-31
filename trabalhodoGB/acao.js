1af6dec3b96bb078e4cfe707a4dc7bea62fa5953
sldrafaell <105649990+sldrafaell@users.noreply.github.com>
2023-05-31 08:50:01 -0300

    Add files via upload

--- trabalhodoGB/acao.js
+++ trabalhodoGB/acao.js
@@ -1,4 +1,6 @@
 var numero = [];
+var quantidadeNumeros = 6;
+var sorteio = [];
 
 $(document.querySelector('#botao')).click(function(){
     
@@ -7,11 +9,15 @@ $(document.querySelector('#botao')).click(function(){
     result.innerHTML = numero;
 
     for (i=0;i < 6; i++){
-        ale = Math.floor(Math.random() * 100) + 1;
-        
-        numero.push(ale);
+        while (numero.length < quantidadeNumeros){
+            ale = Math.floor(Math.random() * 100) + 1;
+            if ($.inArray(ale, numero) === -1){
+                numero.push(ale);
+                
+            } 
+        }
         result.innerHTML = "<p>"+numero+"</p>";
-
+        
     }
     
 });
